0dc4dbfe7297779a72cfe9869032dbdd4ecc6337
Taft Sanders <taftsanders@gmail.com>
2023-08-30 11:55:07 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("taftsanders is Awesome!!!", "larry3d", "brown", true)
+	myFigure := figure.NewColorFigure("taftsanders is Awesome!!!", "larry3d", "red", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
